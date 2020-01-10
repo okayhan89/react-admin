ac91e6756d0368c428d0ae0f763d7f75386652c0
한승엽(HAN SEOUNG YEOB)/통신/미디어Digital추진그룹/SK <09707@skcc.com>
2020-01-10 16:07:07 +0900

    firt commit

---
+++ src/views/Test/Test.js
@@ -0,0 +1,15 @@
+/*eslint-disable*/
+import React from "react";
+import GridContainer from "components/Grid/GridContainer.js";
+import Card from "components/Card/Card.js";
+
+export default function Test() {
+    return (
+        <div>
+            <div>This is Test Page for my practices</div>
+            <GridContainer>
+              <Card>hihi</Card>
+            </GridContainer>    
+        </div>
+    );
+}
